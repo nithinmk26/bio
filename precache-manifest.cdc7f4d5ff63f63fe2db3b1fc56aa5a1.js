638c70e0012c3309594bcde566fbc7b7ee2c67a9
NITHIN M K <nithinmk26@gmail.com>
2024-03-27 19:05:19 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.cdc7f4d5ff63f63fe2db3b1fc56aa5a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d176b564b027efdbe53f",
+    "url": "/react-gh-pages/static/css/main.d250fa2b.chunk.css"
+  },
+  {
+    "revision": "d176b564b027efdbe53f",
+    "url": "/react-gh-pages/static/js/main.d176b564.chunk.js"
+  },
+  {
+    "revision": "d5a82794cb252de57f41",
+    "url": "/react-gh-pages/static/js/1.d5a82794.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "33360c9f5d2a49101ad8887266f3c436",
+    "url": "/react-gh-pages/index.html"
+  }
+];
