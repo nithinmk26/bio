4a948289fc29de4aa51f4c346517169d5e785a46
NITHIN M K <nithinmk26@gmail.com>
2024-03-27 19:00:56 +0530

    updated new files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Header';
 import About from './components/About';
 import Resume from './components/Resume';
 import Portfolio from './components/Portfolio';
-import Testimonials from  './components/Testimonials';
 import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
 import resumeData from './resumeData';
