a28543f533cf3b738284e6ccf38412d00acf2811
NITHIN M K <nithinmk26@gmail.com>
2024-04-03 19:00:03 +0530

    Updates

---
+++ precache-manifest.8b49b72e5157952009f2e92d34a284ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1012000de5db7b9ab10",
+    "url": "/bio/static/css/main.1972b50d.chunk.css"
+  },
+  {
+    "revision": "e1012000de5db7b9ab10",
+    "url": "/bio/static/js/main.e1012000.chunk.js"
+  },
+  {
+    "revision": "2de4c4bbdaa8a5c3a7f1",
+    "url": "/bio/static/js/1.2de4c4bb.chunk.js"
+  },
+  {
+    "revision": "34252642cbce0f840bda",
+    "url": "/bio/static/js/runtime~main.34252642.js"
+  },
+  {
+    "revision": "d649bcdcadedebfa6193bd621953e6c0",
+    "url": "/bio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bio/precache-manifest.838479f592eef76fff0ce4389c663f79.js"
+  "/bio/precache-manifest.8b49b72e5157952009f2e92d34a284ac.js"
 );
 
 workbox.clientsClaim();
