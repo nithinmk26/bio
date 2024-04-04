00d2e317832e20b66b12f32f85fcc8863f8dc111
NITHIN M K <nithinmk26@gmail.com>
2024-04-04 23:16:38 +0530

    updated contactdetails sectin

--- src/components/About.js
+++ src/components/About.js
@@ -15,28 +15,11 @@ export default class About extends Component {
             <div className="nine columns main-col">
 
                <h2>About Me</h2>
-               <p>
-               {
-                 resumeData.aboutme
-               }
-               </p>
-
-               <div className="row">
-
-                  <div className="columns contact-details">
-
-                  <h2>Contact Details</h2>
-                  <p className="address">
-       						<span>{resumeData.name} s/o {resumeData.fatherName}</span>
-                     <br></br>
-       						   <span>
-                     {resumeData.address}
-                    </span>
-                    <br></br>
-                    <span>{resumeData.website}</span>
-       					   </p>
-                  </div>
-               </div>
+                <p>
+                {
+                  resumeData.aboutme
+                }
+                </p>
             </div>
          </div>
       </section>
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,25 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 export default class ContactUs extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
       <section id="contact">
-          <div className="row section-head">
-            <div className="ten columns">
-              <h3 className="lead">
+        <div className="row section-head">
+          <div className="ten columns">
+            <h3 className="lead">
               Feel free to contact for any further information
-              </h3>
+            </h3>
+          </div>
+        </div>
+        <div className="row">
+          <aside className="eigth columns footer-widgets">
+            <div className="widget">
+              <h4>
+                <p className="address">
+                  <span>
+                    {resumeData.name} s/o {resumeData.fatherName}
+                  </span>
+                  <br />
+                  <span>{resumeData.address}</span>
+                  <br />
+                </p>
+              </h4>
+              <h4>Nithin : 9986024343, 9113683980</h4>
+              <h4> Mother : 7892041457</h4>
             </div>
-          </div>
-          <div className="row">
-            <aside className="eigth columns footer-widgets">
-              <div className="widget">
-                <h4>Nithin : 9986024343,  9113683980</h4>
-                 <h4> Mother : 7892041457</h4>
-              </div>
-            </aside>
-          </div>
-        </section>
-        );
+          </aside>
+        </div>
+      </section>
+    );
   }
 }
