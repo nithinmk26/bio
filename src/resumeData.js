91c108246df04816c3b1015f3cd657c57db1e334
NITHIN M K <nithinmk26@gmail.com>
2024-04-23 18:40:10 +0530

    update home number

--- src/resumeData.js
+++ src/resumeData.js
@@ -26,7 +26,7 @@ let resumeData = {
         }
       ],
     "aboutme":" Driven by curiosity and a passion for learning, I am always eager to explore new ideas, cultures, and experiences. Dedicated and accomplished professional currently serving as a Senior Product Developer at Harman, a renowned technology company. With a Bachelor of Engineering degree under my belt, I have cultivated a passion for innovation and problem-solving, driving my success in the dynamic field of technology. Family holds a sacred place in my life, where love and support thrive. With a retired teacher as my father and my mother, who currently teaches and is approaching retirement within the next three years. My elder brother, happily married and has a child, adds to the joy and unity we share as a family.",
-    "address":"Chethana Nilaya, Rajajinagar Birur Kadur (T) Chickmagaluru (D) - 577116",
+    "address":" #199/11, Chethana Nilaya, Rajajinagar Birur Kadur (T) Chickmagaluru (D) - 577116",
     "education":[
       {
         "UniversityName":"BE - CSE",
